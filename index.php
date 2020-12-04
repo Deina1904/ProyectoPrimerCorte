e0de60441f2f5dace95f7ff50ab846169a206b1f
unknown <moren@PC-VYG0238-2.intranet.vygts.com>
2020-12-03 21:39:56 -0500

    TrebajoProAvan

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Mundo Capitan America</title>
+  <meta charset="utf-8"> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+    .fakeimg {
+      height: 200px;
+      background: #aaa;
+    }
+  </style>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:50px;margin-top: 50px;margin-right: 50px;margin-left: 50px;">
+  <h1>CAPITAN AMERICA</h1>
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark" style="margin-right: 50px;margin-left: 50px;">
+  <a class="navbar-brand" href="#">Menu Registros -> </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="nav-link" href="personas.php">PERSONAS SALVADAS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="mision.php">MISIONES</a>
+      </li> 
+      
+      <li class="nav-item">
+        <a class="nav-link" href="patrocinadores.php">PATROCINADORES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="enemigo.php">ENEMIGOS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="equipo.php">COMPAÑEROS DE EQUIPO </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="aliados.php">ALIADOS</a>
+      </li>
+      
+    </ul>
+  </div>  
+</nav>  
+
+<div class="jumbotron text-center" style="margin-bottom:0;margin-right: 50px;margin-left: 50px;margin-top: 25px;">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6 ">
+      <img src="img\capitanAMerica.jpg" class="rounded" alt="Cinque Terre"> 
+      </div>
+      <div class="col-sm-6 ">
+        <div class="form-group">
+        <p style="margin-top: 11%;">
+             Nombre Auténtico: Steven "Steve" Rogers <br>
+            Ocupación: Heroe, ex-soldado de la Segunda Guerra Mundial <br>
+            Identidad: Secreta <br>
+            Situación legal: Ciudadano de los USA sin antecedentes penales <br>
+            Otros "Alias": "Capi", "Cabeza Alada", Nómada, el Capitán, Steven Grant Rogers <br>
+            Lugar de Nacimiento: New York City <br>
+            Estado Civil: Soltero 
+          </p>
+        </div> 
+      </div>
+    </div>
+  </div>
+  <p style="margin-top: 5%;">
+    Steve Rogers nació durante la Depresión y creció, convirtiéndose en un joven débil en una familia pobre. 
+    Su padre murió cuando él era un niño; su madre, durante la adolescencia de Steve. 
+    Horrorizado ante un noticiario que mostraba a los nazis en Europa, Rogers intentó alistarse en el ejército. 
+    En cambio, debido a su fragilidad y su malestar, fue rechazado. 
+    Oyendo la súplica fervorosa del chico para que lo aceptaran, 
+    el General Chester Phillips del Ejército de USA ofreció a Rogers la oportunidad de formar 
+    parte de un experimento especial llamado Operación Renacimiento. 
+    Rogers aceptó y fue llevado a un laboratorio secreto en Washington D.C., donde fue presentado al 
+    Dr. Abraham Erskine (nombre clave: Profesor Reinstein), el creador de la fórmula del Supersoldado. 
+  </p> 
+</div>
+
+</body>
+</html>
